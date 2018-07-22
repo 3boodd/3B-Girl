b018e2a168602ef6b3062db138bea551f1d7c259
3boodd <36028899+3boodd@users.noreply.github.com>
2018-07-22 18:17:34 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+client.on('voiceStateUpdate', (Codes, ReBeL) => {
+    let channel3 = ["469575710558650388"];
+client.channels.get(channel3)
+ReBeL.guild.member(ReBeL).addRole(ReBeL.guild.roles.find("name", "بنت")).then(r => {
+      });
+console.log("Done");
+});
+  client.on('voiceStateUpdate', (codes, ReBeL) => {
+if(ReBeL.voiceChannelID !== "469575710558650388") return console.log("أيرور . ");
+ReBeL.guild.createChannel(ReBeL.user.username , 'voice').then((rebeeel) =>{
+ReBeL.guild.members.get(ReBeL.id).setVoiceChannel(rebeeel.id).then((codess) =>{
+  console.log("تــــــم .");
+  let scan = setInterval(()=>{
+  rebeeel.delete();
+  }, 305);
+});
+});
+});
+
+
+
+client.login(process.env.BOT_TOKEN);  //اياكككك تلعب هنا لا تحط توكنك هنا 
