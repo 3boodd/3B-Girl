851a5dd419294ed3268aeed842ee900e8827a7fc
3boodd <36028899+3boodd@users.noreply.github.com>
2018-07-24 18:20:49 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,4 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-        
-const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 const { Client, Util } = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
